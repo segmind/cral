cf8aab659b7b17029a2a715c239379870a94e7ce
shreeram0206 <57616534+shreeram0206@users.noreply.github.com>
2020-11-12 15:40:49 +0530

    instance seg pipeline integrate

--- cral/pipeline/__init__.py
+++ cral/pipeline/__init__.py
@@ -1,3 +1,4 @@
 from .core import ClassificationPipe
 from .object_detection_pipeline import ObjectDetectionPipe
 from .semantic_segmentation_pipeline import SemanticSegPipe
+from .instance_segmentation_pipeline import InstanceSegPipe
