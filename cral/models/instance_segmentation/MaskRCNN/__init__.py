5b063cbd6b4a21ca10b884a2a68030f563c55ca5
shreeram0206 <57616534+shreeram0206@users.noreply.github.com>
2020-11-12 15:30:14 +0530

    maskrcnn integrate

---
+++ cral/models/instance_segmentation/MaskRCNN/__init__.py
@@ -0,0 +1,8 @@
+from .utils import (MaskRCNNConfig,
+                    log_MaskRCNN_config_params,
+                    MaskRCNNPredictor)
+from .maskrcnn import create_MaskRCNN
+from .tfrecord_parser import MaskRCNNGenerator
+from .instance_seg_utils import *
+from .mrcnn_utils import *
+from .parsing_utils import *
