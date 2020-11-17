de65f10da311415deaaa1e9f5b5eee1467a04793
shreeram0206 <57616534+shreeram0206@users.noreply.github.com>
2020-11-17 17:40:11 +0530

    maskrcnn integrate

---
+++ cral/models/instance_segmentation/__init__.py
@@ -0,0 +1,3 @@
+from .MaskRCNN import (MaskRCNNConfig, MaskRCNNGenerator, MaskRCNNPredictor,
+                       create_MaskRCNN, log_MaskRCNN_config_params)
+from .utils import SparseMeanIoU, annotate_image
