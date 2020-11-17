20132f2b592e06d869a210a1984a624b52dbeac0
shreeram0206 <57616534+shreeram0206@users.noreply.github.com>
2020-11-17 17:37:11 +0530

    maskrcnn integrate

--- cral/data_versioning/__init__.py
+++ cral/data_versioning/__init__.py
@@ -5,6 +5,7 @@ from .classification_data_parse_v2 import \
 from .ObjectDetection_parse_data_v2 import \
     make_csv as objectDetection_dataset_hasher
 from .segmentation_data_parse_v2 import make_csv as segmentation_dataset_hasher
+from .instanceSeg_data_parse_v2 import make_csv as instanceSeg_dataset_hasher
 
 
 def log_classification_dataset(*args, **kwargs):
@@ -50,3 +51,21 @@ def log_object_detection_dataset(*args, **kwargs):
     """
     dataset_hash, dataset_csv_path, dataset_json = objectDetection_dataset_hasher(
         tempfile.gettempdir(), *args, **kwargs)
+
+
+def log_instance_segmentation_dataset(*args, **kwargs):
+    """Parses the instance segmentation data and logs to tracking server
+
+    Args:
+        annotation_format (str): one of 'coco'
+        train_images_dir (str): path to images
+        train_anno_dir (str): path to annotation
+        img_to_anno (function, optional): Function to convert image name to annotation name
+        val_images_dir (str, optional): path to validation images
+        val_anno_dir (str, optional): path to vallidation annotation
+        split (float, optional): float to divide training dataset into training and val
+    """
+    dataset_hash, dataset_csv_path, dataset_json = instanceSeg_dataset_hasher(
+        tempfile.gettempdir(), *args, **kwargs)
+    try_mlflow_log(log_artifact, key='dataset_versioned.csv', path=dataset_csv_path)
+    try_mlflow_log(log_artifact, key='dataset_meta.json', path=dataset_json)
